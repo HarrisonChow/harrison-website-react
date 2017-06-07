b8edb443736356b264ce90d430530c3529880cb0
Airwoman <airwomanzhy@hotmail.com>
2017-06-08 01:09:10 +1000

    setting environment

---
+++ app/js/Views/About/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class About extends Component {
+  render() {
+
+    return (
+      <div className='About'>
+        fff
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  context: __dirname + "/app",
+
+  entry: {
+    javascript: "./js/app.js",
+    html: "./index.html",
+  },
+
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist",
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      }
+    ]
+  }
+};
